7dd673548b7426fca8dcdde5145c46274a27f189
qingwa2009 <qingwa2009@outlook.com>
2021-10-07 22:25:07 +0800

    lala

---
+++ rootfs/init.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("\e[32m++++++++++++++++++++hello arm!++++++++++++++++\e[0m\n");
+    sleep(10);
+    // while (1)
+    // {
+    // }
+    return 0;
+}
